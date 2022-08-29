63ec758306f0ce087670a9eddfb6ee67944f12c0
Tom Shaw <Tomshaw650@gmail.com>
2022-08-29 23:46:30 +0100

    adding basic title and image + assets + configs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      colors: {
+        "main": "#191717",
+        "accent": "#03989e",
+      }
+    },
 	},
 	plugins: [],
 }
