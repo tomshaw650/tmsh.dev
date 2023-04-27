afef4aaa2475da9b6c32b3561ecbe2ceb371ee69
Blue Avocado <tomshaw650@gmail.com>
2023-04-27 10:33:44 +0100

    hover on social buttons

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,22 +6,22 @@ export const Footer = () => {
       <ul className="flex gap-x-5 bottom-0">
         <li>
           <a href="https://www.linkedin.com/in/tom-shaw99/">
-            <SiLinkedin size={32} />
+            <SiLinkedin size={32} className="hover:text-[#FDD641]" />
           </a>
         </li>
         <li>
           <a href="https://www.github.com/tomshaw650">
-            <SiGithub size={32} />
+            <SiGithub size={32} className="hover:text-[#FDD641]" />
           </a>
         </li>
         <li>
           <a href="https://www.twitter.com/tomshawdev">
-            <SiTwitter size={32} />
+            <SiTwitter size={32} className="hover:text-[#FDD641]" />
           </a>
         </li>
         <li>
           <a href="https://discordapp.com/users/153567788303122432">
-            <SiDiscord size={32} />
+            <SiDiscord size={32} className="hover:text-[#FDD641]" />
           </a>
         </li>
       </ul>
